3547d23c007e198bed630d9f4c9839629858a007
Cosmic Open Source Projects <rodeboys5@gmail.com>
2015-01-11 18:54:53 -0500

    update 0.2

--- pyfancy.py
+++ pyfancy.py
@@ -1,14 +1,26 @@
-#Pyfancy 0.1 by Cosmic Open Source Projects learn more at https://github.com/ilovecode1/pyfancy
+#Pyfancy 0.2 by Cosmic Open Source Projects learn more at https://github.com/ilovecode1/pyfancy
 
 class pyfancy:
 
 	END = '\033[0m'
+	PURPLE='\033[35m'
+        CYAN='\033[36m'
+        LIGHTGREY='\033[37m'
+        DARKGREY='\033[90m'
+        LIGHTRED='\033[91m'
+        LIGHTGREEN='\033[92m'
+        LIGHTBLUE='\033[94m'
+        PINK='\033[95m'
 	BLUE = '\033[94m'
 	GREEN = '\033[92m'
 	YELLOW = '\033[93m'
 	RED = '\033[91m'
 	BOLD = '\033[1m'
 	UNDERLINE = '\033[4m'
-
+	REVERSE='\033[07m'
+        STRIKETHROUGH='\033[09m'
+        INVISABLE='\033[08m'
+        
+        
 def demo():
-	print(pyfancy.BLUE + “H” + pyfancy.END + pyfancy.GREEN + “e” + pyfancy.END + pyfancy.YELLOW + “l” + pyfancy.END + pyfancy.RED + “l” + pyfancy.END + pyfancy.BOLD + “o” + pyfancy.END + pyfancy.UNDERLINE + pyfancy.END)
+	print(pyfancy.RED + "HELLO RED!" + pyfancy.END)
