1b5a0ec385a3156c51cf4f6f94563051ec709f3c
Cosmic Open Source Projects <rodeboys5@gmail.com>
2015-01-18 11:25:01 -0500

    update 0.2.5

--- pyfancy.py
+++ pyfancy.py
@@ -1,4 +1,16 @@
-#Pyfancy 0.2 by Cosmic Open Source Projects learn more at https://github.com/ilovecode1/pyfancy
+#Pyfancy 0.2.5 by Cosmic Open Source Projects learn more at https://github.com/ilovecode1/pyfancy
+
+documentation = print("Type documentation() for help or go to https://github.com/ilovecode1/pyfancy!")
+
+def documentation(input=None):
+	
+	if (input == None):
+		
+		print('''BLUE\nprint (pyfancy.BLUE + "Hello Blue!" + pyfancy.END)\nGREEN\nprint (pyfancy.GREEN + "Hello Green!" + pyfancy.END)\nYELLOW\nprint (pyfancy.YELLOW + "Hello Yellow!" + pyfancy.END)\nRED\nprint (pyfancy.RED + "Hello Red!" + pyfancy.END)\nPINK\nprint (pyfancy.PINK + "Hello Pink!" + pyfancy.END)\nLIGHTBLUE\nprint (pyfancy.LIGHTBLUE + "Hello Lightblue!" + pyfancy.END)\nLIGHTGREEN\nprint (pyfancy.LIGHTGREEN + "Hello Lightgreen!" + pyfancy.END)\nLIGHTRED\nprint (pyfancy.LIGHTRED + "Hello Lightred!" + pyfancy.END)\nLIGHTGREY\nprint (pyfancy.LIGHTGREY + "Hello Lightgrey!" + pyfancy.END)\nDARKGREY\nprint (pyfancy.DARKGREY + "Hello Darkgrey!" + pyfancy.END)\nCYAN\nprint (pyfancy.CYAN + "Hello Cyan!" + pyfancy.END)\nPURPLE\nprint (pyfancy.PURPLE + "Hello Purple!" + pyfancy.END)\nBOLD\nprint (pyfancy.BOLD + "Hello Bold!" + pyfancy.END)\nUNDELINE\nprint (pyfancy.UNDERLINE + "Hello Underline!" + pyfancy.END)\nREVERSE\nprint (pyfancy.REVERSE + "Hello Reverse!" + pyfancy.END)\nSTRIKETHROUGH\nprint (pyfancy.STRIKETHOUGH + "Hello Strikethough!" + pyfancy.END)\nINVISABLE\nprint (pyfancy.INVISABLE + "Hello Invisable!" + pyfancy.END)''')
+    
+    else:
+    	
+    	print(input + " is not avalible!")
 
 class pyfancy:
 
