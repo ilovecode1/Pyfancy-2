3c3ed0bdd2aeae2cf1742fa86ca6c521931f23d0
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2015-02-21 14:58:21 -0500

    v0.5
    Pyfancy Now use Pylaunch API to make Pyfancy even smarter!

--- pyfancy.py
+++ pyfancy.py
@@ -1,4 +1,24 @@
-#Pyfancy 0.4 by Cosmic Open Source Projects learn more at https://github.com/ilovecode1/pyfancy
+#Pyfancy 0.5 by Cosmic Open Source Projects learn more at https://github.com/ilovecode1/pyfancy
+
+import sys
+
+def idleorcommandline():
+   'If idleorcommandline returns True its IDLE if False its command line!'
+   a = sys.executable
+   m = '\\\\'
+   m = m[0]
+   while True:
+       b = len(a)
+       c = a[(b - 1)]
+       if c == m:
+           break
+       a = a[:(b - 1)]
+   if sys.executable == a + 'pythonw.exe':
+       return True
+   else:
+       return False
+       
+get = idleorcommandline()
 
 documentation = ("Type documentation() for help or go to https://github.com/ilovecode1/pyfancy!")
 
@@ -14,28 +34,53 @@ def documentation(input=None):
 
 class pyfancy:
         
-	END='\033[0m'
-	INVISABLE2='\033[02m'
-	PURPLEBLUE='\033[34m'
-	WHITETEXTBLACKBACKROUND='\033[07m'
-        MISTGREY='\033[20m'
-        PURPLE='\033[35m'
-        CYAN='\033[36m'
-        LIGHTGREY='\033[37m'
-        DARKGREY='\033[90m'
-        LIGHTRED='\033[91m'
-        LIGHTGREEN='\033[92m'
-        LIGHTBLUE='\033[94m'
-        PINK='\033[95m'
-	BLUE = '\033[94m'
-	GREEN = '\033[92m'
-	YELLOW = '\033[93m'
-	RED = '\033[91m'
-	BOLD = '\033[1m'
-	UNDERLINE = '\033[4m'
-	REVERSE='\033[07m'
-        STRIKETHROUGH='\033[09m'
-        INVISABLE='\033[08m'
+        if (get):
+		END=''
+		INVISABLE2=''
+		PURPLEBLUE=''
+		WHITETEXTBLACKBACKROUND=''
+	        MISTGREY=''
+	        PURPLE=''
+	        CYAN=''
+	        LIGHTGREY=''
+	        DARKGREY=''
+	        LIGHTRED=''
+	        LIGHTGREEN=''
+	        LIGHTBLUE=''
+	        PINK=''
+		BLUE = ''
+		GREEN = ''
+		YELLOW = ''
+		RED = ''
+		BOLD = ''
+		UNDERLINE = ''
+		REVERSE=''
+	        STRIKETHROUGH=''
+	        INVISABLE=''
+        	
+        else:
+		END='\033[0m'
+		INVISABLE2='\033[02m'
+		PURPLEBLUE='\033[34m'
+		WHITETEXTBLACKBACKROUND='\033[07m'
+	        MISTGREY='\033[20m'
+	        PURPLE='\033[35m'
+	        CYAN='\033[36m'
+	        LIGHTGREY='\033[37m'
+	        DARKGREY='\033[90m'
+	        LIGHTRED='\033[91m'
+	        LIGHTGREEN='\033[92m'
+	        LIGHTBLUE='\033[94m'
+	        PINK='\033[95m'
+		BLUE = '\033[94m'
+		GREEN = '\033[92m'
+		YELLOW = '\033[93m'
+		RED = '\033[91m'
+		BOLD = '\033[1m'
+		UNDERLINE = '\033[4m'
+		REVERSE='\033[07m'
+	        STRIKETHROUGH='\033[09m'
+	        INVISABLE='\033[08m'
         
         
 def demo():
