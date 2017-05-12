270b1cf4f48bf35b7b937880f9d27c4ab4551d0e
Cosmic Web Services <andrewrodebaugh@cosmicsearch.org>
2017-05-12 12:14:24 -0400

    Update pyfancy.py

--- pyfancy.py
+++ pyfancy.py
@@ -20,7 +20,7 @@
 # The get() method is better for accessing text because it resets the text
 # style so no new text will have unwanted styling.
 
-class pyfancy:
+class pyfancy(parseText = ""):
     def __str__(self): return self.get()
     def __init__(self, obj=""):
         # Stores output text, for reset use get()
@@ -54,6 +54,9 @@ class pyfancy:
 
     # Stores output text, for reset use get()
     out = ""
+    
+    if (parseText != ""):
+        return parse(parseText)
 
     # Returns output text and resets properties
     def get(self):
