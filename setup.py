174386ee3670b8885789a123fe3699ae0ad279cc
Kristinita <SashaChernykhEmpressOfUniverse@kristinita.ru>
2018-01-19 09:28:16 +0300

    [Add] PyPI installation files

---
+++ setup.py
@@ -0,0 +1,26 @@
+"""A setuptools based setup module.
+
+See:
+https://packaging.python.org/en/latest/distributing.html
+https://github.com/pypa/sampleproject
+"""
+
+# Always prefer setuptools over distutils
+from setuptools import setup
+
+# For Markdown README
+# https://stackoverflow.com/a/26737672/5951529
+try:
+    import pypandoc
+    long_description = pypandoc.convert('README.md', 'rst')
+except(IOError, ImportError):
+    long_description = open('README.md').read()
+
+setup(
+    # Although 'package_data' is the preferred approach, in some case you may
+    # need to place data files outside of your packages. See:
+    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
+    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
+    data_files=[('', ['README.md'])],
+    long_description=long_description
+)
